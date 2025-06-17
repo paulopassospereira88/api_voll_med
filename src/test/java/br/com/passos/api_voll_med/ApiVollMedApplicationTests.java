9d80b0f7bfed8441c911742d9c27b25dbfd64b64
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-06-17 08:22:54 -0300

    criacao do projeto

---
+++ src/test/java/br/com/passos/api_voll_med/ApiVollMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passos.api_voll_med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVollMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
