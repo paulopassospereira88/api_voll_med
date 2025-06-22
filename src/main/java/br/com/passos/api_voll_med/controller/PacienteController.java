74898fa3dbcd75a1e716d1cf3d639a1574b37601
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-06-22 17:19:28 -0300

    implementado o controller para gerenciamento de pacientes

---
+++ src/main/java/br/com/passos/api_voll_med/controller/PacienteController.java
@@ -0,0 +1,52 @@
+package br.com.passos.api_voll_med.controller;
+
+import br.com.passos.api_voll_med.paciente.*;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    private final PacienteRepository pacienteRepository;
+
+    public PacienteController(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
+        pacienteRepository.save(new Paciente(dadosCadastroPaciente));
+        return  ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+       Page<DadosListagemPaciente> pacientes = pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(pacientes);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<DadosListagemPaciente> atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dadosAtualizacaoPaciente) {
+        var paciente = pacienteRepository.getReferenceById(dadosAtualizacaoPaciente.id());
+        paciente.atualizarInformacoes(dadosAtualizacaoPaciente);
+        pacienteRepository.save(paciente);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new DadosListagemPaciente(paciente));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.excluir();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
