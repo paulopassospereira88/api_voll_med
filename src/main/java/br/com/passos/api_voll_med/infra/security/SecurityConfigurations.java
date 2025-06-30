95491e79fb2c8d4c53890f8c3c50f6503d3f7b89
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-06-30 19:23:43 -0300

    Adicionar filtro de autenticação JWT
    375 / 5.000
    Introduz o SecurityFilter para processar tokens JWT em cada solicitação, extraindo o usuário e definindo a autenticação no contexto de segurança. Atualiza o SecurityConfigurations para registrar o filtro e restringe o acesso a usuários autenticados, exceto para /login. O TokenService agora fornece um método para extrair o usuario de um JWT, melhorando a validação de tokens e a autenticação do usuário.

--- src/main/java/br/com/passos/api_voll_med/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/passos/api_voll_med/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.passos.api_voll_med.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     private final AuthenticationConfiguration authenticationConfiguration;
 
@@ -28,6 +33,10 @@ public class SecurityConfigurations {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/login").permitAll();
+                    req.anyRequest().authenticated();
+                }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/br/com/passos/api_voll_med/infra/security/TokenService.java
+++ src/main/java/br/com/passos/api_voll_med/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.passos.api_voll_med.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
@@ -26,6 +27,19 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException ex){
             throw new RuntimeException("Erro ao gerar token JWT", ex);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API VOLL MED")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException ex){
+            throw new RuntimeException("Erro ao validar token JWT", ex);
         }
     }
 
